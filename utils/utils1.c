ff212602a7db4c8e566527b2dbcd29e62fab3d3a
Mahaut Latinis <malatini@student.42.fr>
2021-03-10 20:50:41 +0100

    save

--- utils/utils1.c
+++ utils/utils1.c
@@ -5,12 +5,10 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: malatini <malatini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/09 14:03:52 by malatini          #+#    #+#             */
-/*   Updated: 2021/03/10 19:22:16 by malatini         ###   ########.fr       */
+/*   Created: 2021/03/10 20:49:33 by malatini          #+#    #+#             */
+/*   Updated: 2021/03/10 20:49:39 by malatini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "../ft_printf.h"
 
 void	ft_putchar(char c)
 {
@@ -22,43 +20,4 @@ int	ft_isdigit(int c)
 	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
-}
-
-int	found_char(const char *str, char c)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	is_correct_type(char c)
-{
-	if (c == 'i' || c == 'd' || c == 'u' || c == 's' || c == 'c' ||
-		c == 'x' || c == 'X' || c == 'p' || c == '%')
-	{
-		return (1);
-	}
-	return (0);
-}
-
-void	ft_putnbr(int nbr)
-{
-	long n;
-
-	n = nbr;
-	if (nbr < 0)
-	{
-		n = -n;
-		ft_putchar('-');
-	}
-	if (n > 9)
-		ft_putnbr(n / 10);
-	ft_putchar(n % 10 + '0');
 }
